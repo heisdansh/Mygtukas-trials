728bb751226f55943182a7e9028243d965a32250
heisdansh <63120880+heisdansh@users.noreply.github.com>
2021-08-29 22:41:31 +0300

    Working color change

--- app/javascript/controllers/styling_controller.js
+++ app/javascript/controllers/styling_controller.js
@@ -1,14 +1,27 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "button" ]
-
+  static targets = [ "button" ];
+  //var currentColor;
   connect() {
-      this.buttonTarget.classList.add("bg-black");
+      //currentColor="bg-gray-400";
+      this.buttonTarget.classList.add("bg-gray-600");
       this.buttonTarget.classList.add("text-white");
   }
   clicked() {
-    this.buttonTarget.classList.toggle("bg-black");
-    this.buttonTarget.classList.toggle("text-white");
+    this.buttonTarget.className = "";
+    var colorArray = [
+      'gray',
+      'red',
+      'yellow',
+      'green',
+      'blue',
+      'indigo',
+      'purple',
+      'pink'
+    ];
+    var randomNumber = Math.floor(Math.random()*colorArray.length);
+    //this.buttonTarget.classList.add("bg-black");
+    this.buttonTarget.className=`p-5 text-lg font-bold rounded-md bg-${colorArray[randomNumber]}-600`;
 }
 }
