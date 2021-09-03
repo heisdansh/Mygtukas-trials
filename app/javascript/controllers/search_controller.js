b33b945498b6ebdfbd3dc8004270a7842c395854
Elvinas <elvinasnet@gmail.com>
2021-09-03 17:52:24 +0300

    adding fix suggestions; routes rewrite; migrating tailwind from views to (application) layout

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,35 +1,34 @@
-import { Controller } from "stimulus"
+import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = [ "input", "result"];
+  static targets = ['input', 'result']
 
-  connect(){
+  connect () {}
 
+  // Šito metodo išvis nereikia
+  focusInput () {
+    this.inputTarget.innerText = ''
+    this.inputTarget.focus()
   }
-  focusInput(){
-    this.inputTarget.innerText="";
-    this.inputTarget.focus();
+
+  // Tu turėtum vykdyti paiešką čia.
+  handleInput () {
+    if (this.inputTarget.value === '') return // Kad nevykdytų jokios logikos, jei input'as tuščias. Taip pat == nėra tas pats, kas === :)
+
+    // Čia reiktų cikliuko kuris patogglina 'hidden' klasę ant kiekvienos įvesties.
+    // this.resultTargets.forEach......
+
+    // Pamąstymui.
+    // Tavo results elementai viduj turi didžiasias raides (ir tai nėra blogai).
   }
-  handleInput(){
-    if (this.inputTarget.innerText!="")
-    {
-        console.log(this.inputTarget.innerText);
+  mark () {
+    this.resultTargets.forEach(element => {
+      this.element.classList.add('bg-purple-600') // Čia viskas veiktų, jeigu sutrumpintum šitą eilutę penkiais simboliais. 
+    })
+
+    // Viskas gerai, bet šiaip, jeigu galima išvengti tokių C++'inių ciklų - venk.
+    for (let a = 0; a <= this.resultTargets.length; a++) {
+      this.resultTargets[a].classList.add('bg-purple-600')
     }
-    this.display;
   }
-  display(){
-      alert(this.resultTargets);
-  }
-  mark(){
-    alert(this.resultTargets.length);
-      //this.resultTargets[2].classList.add("bg-purple-600");
-     this.resultTargets.forEach(element => {
-        this.element.classList.add("bg-purple-600");
-          });
-    for (let a=0; a<=this.resultTargets.length; a++)
-    {
-        this.resultTargets[a].classList.add("bg-purple-600");
-    }
-  }
-
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'search/index'
-  get 'button/index'
+  # Kad linkai būtų normalūs, ir žinotum kaip vyksta.
+  get 'search', to: "search#index"
+  get 'button', to: "button#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
